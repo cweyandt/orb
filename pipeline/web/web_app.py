da9d2ffb09ea5ec862da79a86d69663608a78f34
Chris Weyandt <cweyandt@berkeley.edu>
2021-06-20 13:57:45 -0500

    fixing port mappings for web/flask

--- pipeline/web/web_app.py
+++ pipeline/web/web_app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/web/html')
 
 @app.route('/')
-def render_static():
+def index():
     return render_template('index.html')
 
-# @app.route('/<string:page_name>/')
-# def render_static(page_name):
-#     return render_template('%s.html' % page_name)
+@app.route('/assets/<string:page_name>')
+def render_static(page_name):
+    return render_template('static/%s' % page_name)
     
 if __name__ == '__main__':
     app.run()
