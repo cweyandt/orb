ca03c574401912af34d0f2a2a0bbabf04641c190
Chris Weyandt <cweyandt@berkeley.edu>
2021-06-30 11:00:56 -0500

    sample binary_rbf working with API

--- examples/api_test.py
+++ examples/api_test.py
@@ -10,23 +10,13 @@ headers = {'Content-type': 'application/json'}
 params = {"breakpoints": "10"}
 
 wifiData = pd.read_pickle('../data/cleaned/' + 'wifi_ap_data.pkl')
-wifiTotals = wifiData.sum(axis=1)
-wifiTotals = wifiTotals.replace(0, np.nan)
-wifiTotals = wifiTotals.fillna(method='ffill')
+wifiTotals = wifiData.sum(axis=1).replace(0, np.nan).fillna(method='ffill')
 wifiTotals.index = wifiTotals.index.tz_localize(None)
 
 vals = np.array(wifiTotals["2020-6-21 00:00":"2020-6-28 00:00"])
 ts = np.array(wifiTotals["2020-6-21 00:00":"2020-6-28 00:00"].index)
 data= {"ts": ts.tolist(), "val": vals.tolist()}
 
-print(data)
-
-# plt.figure(figsize=(20, 8))
-# plt.title("Wifi Connections - This Week Last Year")
-# plt.plot(wifiTotals["2020-6-22 00:00":"2020-6-23 00:00"])
-
 response = requests.put(url, headers=headers, json=data, params=params)
 
 print(response.json())
-# print(response.url)
-# print(response.request.headers)
--- orb/routers/models.py
+++ orb/routers/models.py
@@ -4,14 +4,15 @@ from typing import Optional
 import ruptures as rpt
 import numpy as np
 from fastapi import FastAPI, Query, Request, APIRouter, Body
+from matplotlib import pyplot as plt
 from pydantic import BaseModel
 from typing import List
+import mpld3
 
 router = APIRouter(
     prefix="/models",
     tags=["models"]
 )
-
 
 class ModelName(str, Enum):
     pelt_rbf = "pelt_rbf"
@@ -31,8 +32,26 @@ def binseg_rbf(breakpoints: int, data: RawData):
     bkps_ts = []
     for i in bkps_i[:-1]:
         bkps_ts.append(data.ts[i])
-    print(bkps_ts)
     return {"bkps_i": bkps_i, "bkps_ts": bkps_ts}
+
+
+@router.put("/binseg_rbf/plot")
+def binseg_rbf(breakpoints: int, data: RawData):
+    points = np.array(data.val)
+    # Binary segmentation search method, RBF segment model
+    algo = rpt.Binseg(model="rbf").fit(points)
+    bkps_i = algo.predict(n_bkps=breakpoints)
+    bkps_ts = []
+    for i in bkps_i[:-1]:
+        bkps_ts.append(data.ts[i])
+    fig, ax_array = rpt.display(points, bkps_i)
+    plt.title('Binary Segmentation Search Method, RBF Segment Model')
+    html_str = mpld3.fig_to_html(fig)
+    Html_file = open("index.html", "w")
+    Html_file.write(html_str)
+    Html_file.close()
+    return {html_str}
+
 
 @router.get("/{model_name}")
 def run_model(model_name: ModelName,
